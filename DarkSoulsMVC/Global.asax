17e6e5131bf3d1d8a9a9520b590007aa0b61185e
JezzyDeves <juncrhepo@gmail.com>
2020-11-16 10:01:29 -0500

    Add project files.

---
+++ DarkSoulsMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DarkSoulsMVC.MvcApplication" Language="C#" %>
---
+++ DarkSoulsMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DarkSoulsMVC.Startup))]
+namespace DarkSoulsMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
