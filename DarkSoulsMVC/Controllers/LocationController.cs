9c7e5277569024da5c0935b41d13205976a00570
JezzyDeves <juncrhepo@gmail.com>
2020-11-17 09:43:57 -0500

    Basic Create and Get for Location

---
+++ DarkSoulsMVC/Controllers/LocationController.cs
@@ -0,0 +1,35 @@
+﻿using Models.LocationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DarkSoulsMVC.Controllers
+{
+    public class LocationController : Controller
+    {
+        // GET: Location
+        public ActionResult Index()
+        {
+            var model = new LocationListItem[0];
+            return View(model);
+        }
+
+        //GET: Location/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Location/Create
+        [HttpPost]
+        public ActionResult Create(LocationCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Services/LocationService.cs
@@ -0,0 +1,37 @@
+﻿using Data;
+using Models.LocationModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class LocationService
+    {
+        private readonly ApplicationDbContext ctx = new ApplicationDbContext();
+        public bool CreateLocation(LocationCreate model)
+        {
+            var entity = new Location()
+            {
+                Name = model.Name,
+                Description = model.Name
+            };
+
+            ctx.Locations.Add(entity);
+            return ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<LocationListItem> GetLocations()
+        {
+            var query = ctx.Locations.Select(e => new LocationListItem
+            {
+                Name = e.Name,
+                Description = e.Description
+            });
+
+            return query.ToArray();
+        }
+    }
+}
